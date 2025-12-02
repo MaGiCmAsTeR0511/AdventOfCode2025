dab0ed78f338378c98ba5aecb318ef3d184d2b98
HOH <kurt.hohenauer@htl-hl.ac.at>
2025-12-02 07:27:32 +0100

    Adventof Code Part 1

---
+++ AdvenOfCode2_1.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+$handle = fopen ("./AdventodCode1_1","r");
+$count = [];
+$number = 50;
+while ( $inhalt = fgets($handle, 4096 ))
+{
+    $length = strlen($inhalt);
+    switch(substr($inhalt,0,1)){
+        case 'L':
+            $number = $number - (int)substr($inhalt ,1,$length);
+            break;
+        case 'R':
+            $number = $number +(int)substr($inhalt,1,$length);
+            break;
+    }
+    $currentPosition = (($number % 100) + 100) % 100;
+    array_key_exists($currentPosition,$count) ? $count[$currentPosition]++ : $count[$currentPosition] = 1;
+}
+$max = [];
+
+echo print_r($count,TRUE)."\n";
+echo print_r((max($count)),TRUE)."\n";
+?>
